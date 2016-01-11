82022fe4acc8dbd28062ba066b26e949e0dc7121
Nicholas Dedenbach <ndedenbach@hotmail.com>
2016-01-11 01:41:36 -0500

    Ripped out even more code

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,18 +6,15 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($timeout) {
     var vm = this;
 
-    vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1451935050197;
-    vm.showToastr = showToastr;
 
     activate();
 
     function activate() {
-      getWebDevTec();
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
